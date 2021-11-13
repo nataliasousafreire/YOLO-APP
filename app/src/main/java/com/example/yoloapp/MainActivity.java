fa5b0a9d430fea3b768bbbe31effe9486ff2813a
Miguel Castro <mcs@icomp.ufam.edu.br>
2021-11-13 10:23:54 -0400

    telaV1

--- app/src/main/java/com/example/yoloapp/MainActivity.java
+++ app/src/main/java/com/example/yoloapp/MainActivity.java
@@ -8,7 +8,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.Toast;
 
@@ -16,14 +18,50 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int VIDEO_REQUEST = 999;
 
-    private ImageButton camButton;
+    //private ImageButton camButton;
+    private ImageView uploadView;
+    private ImageView cameraView;
+    private ImageView arquivosView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camButton = findViewById(R.id.imageButtonCamVideoRecord);
+        //View
+        uploadView = (ImageView) findViewById(R.id.imageViewUploadUndetectedVideo);
+        uploadView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // mudar para tela certo
+                Intent startIntent = new Intent(getApplicationContext(), DetectionActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+        // Cam
+        cameraView = (ImageView) findViewById(R.id.imageViewCamVideoRecord);
+        cameraView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // mudar para tela certo
+                Intent startIntent = new Intent(getApplicationContext(), DetectionActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+        //Arquivos
+        arquivosView = (ImageView) findViewById(R.id.imageViewVideoSamples);
+        arquivosView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // mudar para tela certo
+                Intent startIntent = new Intent(getApplicationContext(), DetectionActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+        //camButton = findViewById(R.id.imageButtonCamVideoRecord);
     }
 
     public void recordVideo( View v){
